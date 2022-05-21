7cfb3259409be7e98c8c752d8df0c954d861f938
jerilyn.wise@paddywax.com <jerilyn.wise@paddywax.com>
2022-05-20 22:05:26 -0500

    update page automatically & dynamic date

--- Work Day Scheduler/assets/script.js
+++ Work Day Scheduler/assets/script.js
@@ -1,14 +1,13 @@
 //this keep the script from running before the DOM is fully loaded
 $(document).ready(function() {
+
+
+    //save description and hour to local storage
     $(".saveBtn").on("click", function() {
         let value = $(this).siblings(".description").val();
         let time = $(this).parent().attr("id");
-        localStorage.setItem("time","value");
+        localStorage.setItem(time,value);
     });
-
-    //add alert that tells them this happned 
-
-
 
     //hour updater
     function updater () {
@@ -16,18 +15,33 @@ $(document).ready(function() {
         $(".time-block").each(function(){
             let hour = parseInt($(this).attr("id").split("-")[1]);
             if (hour<currentTime){
-                $(this).addclass("past");
+                $(this).addClass("past");
             }else if (hour === currentTime) {
                 $(this).removeClass("past");
-                $(this).addclass("present");
+                $(this).addClass("present");
             } else {
                 $(this).removeClass("past");
                 $(this).removeClass("present");
-                $(this).addclass("future");
-                
+                $(this).addClass("future");
             }
-        })
+        });
     }
+
+    updater();
+
+
+    //set interval for updater every 5 mins 
+    setInterval(updater, 5000);
+
+      
+    //dynamically change date using moment
+      $(function() {
+        var date = new Date()
+        $("#currentDay").text(moment().format('dddd, MMMM Do'));
+    });
+
+
+    //bringing every single hour back from local storage. get local storage and bring them into the html so they show.
 
 
 
